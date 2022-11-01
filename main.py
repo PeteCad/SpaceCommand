ddc44d10ba0a77492e5515637d98b55575eff07b
PeteCad <peter.cadoret@gmail.com>
2022-11-01 10:01:27 -0400

    Window and a background!

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+import os
+import time
+import random
+
+# Load image assets
+#enemy ships
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+
+# Player Ship
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+
+# Lasers
+RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellowpng"))
+
+# Background Image
+BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+
